5f989a435ee95ee489a5815d0ffbd3bf88258139
dahliasan <laninaesloco@gmail.com>
2023-11-20 13:47:59 +1100

    Added comments about troubleshooting aniMotum installation

--- scripts/load packages__tracks.R
+++ scripts/load packages__tracks.R
@@ -1,3 +1,18 @@
+
+
+# check if aniMotum library is installed: if not, install it
+if(!require(aniMotum)) {
+  
+  # If macs are facing issues: 
+  # See https://github.com/ianjonsen/aniMotum/issues/42
+  
+  install.packages("aniMotum", 
+                   repos = c("https://cloud.r-project.org",
+                             "https://ianjonsen.r-universe.dev"),
+                   dependencies = TRUE)
+  
+}
+
 # Load libraries for plotting tracks
 library(sf)
 library(rnaturalearth)
